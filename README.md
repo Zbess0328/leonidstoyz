99b957524d4707044f1b25f4ee1ec274f45bfbcf
Zbess0328 <97359933+Zbess0328@users.noreply.github.com>
2022-01-25 19:15:30 -0600

    made changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # leonidstoyz
+const 
---
+++ main.js
@@ -0,0 +1,59 @@
+const MasterChief = {
+    id: 1,
+    name: "Master Chief",
+    maker: "Bungie Inc",
+    operatingSystem: "Xbox",
+    price: 69.95,
+    weight: 169
+}
+const SonicTheHedgehog = {
+    id: 2,
+    name: "Sonic The Hedgehog",
+    maker: "Yuji Naka",
+    operatingSystem: "Sega",
+    price: 96969,
+    weight: 77,
+
+}
+const Pokemon = {
+    id: 3,
+    name: "Pikachu",
+    maker: "Satoshi Tajin",
+    operatingSystem: "Nintendo",
+    price: 69.95,
+    weight: 13,
+
+}
+const toys = [
+    MasterChief,SonicTheHedgehog,Pokemon, 
+]
+console.log(toys);
+
+for (const toy of toys) {
+    console.log(toy)
+}
+//for (const toy of toys){
+    //console.log(`Price is {toy.price} `)
+//}
+const  Matrix= {
+    id: 4,
+    name: "Mr. Anderson",
+    maker: "The Wachowskis",
+    operatingSystem: "Play Station 2",
+    price: 96969,
+    retail: 106969,
+
+}
+const BioShock = {
+    id: 5,
+    name: "Big Daddy",
+    maker: "2k Games",
+    operatingSystem: "Xbox",
+    price: 69.95,
+    retail: 99.99,
+    weight: 2534,
+}
+function addToys(object){
+    object.id = toys.length + 1
+    toys.push(object)
+}
